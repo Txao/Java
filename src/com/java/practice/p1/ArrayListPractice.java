9f0ab80e8c5627be2bac32094ad9aeeb9eeaee85
Txao <txao@pm.me>
2021-02-08 23:27:11 +0300

    Update class ArrayListPractice

---
+++ src/com/java/practice/p1/ArrayListPractice.java
@@ -0,0 +1,18 @@
+package com.java.practice.p1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArrayListPractice {
+    public static void main(String[] args) {
+        List<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < 10; i++)
+            list.add(i);
+
+        list.remove(5);
+
+        System.out.println(list);
+        System.out.println(list.size());
+    }
+}
