1272be8f3dd2c0c517cc552bbe460636514aecd2
Txao <txao@pm.me>
2021-02-10 11:19:06 +0300

    added new methods

--- src/com/java/practice/p2/MyLinkedList.java
+++ src/com/java/practice/p2/MyLinkedList.java
@@ -1,5 +1,7 @@
 package com.java.practice.p2;
 
+import java.util.Arrays;
+
 public class MyLinkedList {
     private Node head;
     private int size;
@@ -17,6 +19,53 @@ public class MyLinkedList {
         }
 
         size++;
+    }
+
+    public int get(int index) {
+        int currentIndex = 0;
+        Node temp = this.head;
+
+        while (temp != null) {
+            if (currentIndex == index)
+                return temp.getValue();
+            else {
+                temp = temp.getNext();
+                currentIndex++;
+            }
+        }
+
+        throw new IllegalArgumentException();
+    }
+
+    public void remove(int index) {
+        int currentIndex = 0;
+        Node temp = this.head;
+
+        while (temp != null) {
+            if ((currentIndex + 1) == index) {
+                temp.setNext(temp.getNext().getNext());
+                size--;
+                return;
+            }
+            else {
+                temp = temp.getNext();
+                currentIndex++;
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        int[] result = new int[this.size];
+        int index = 0;
+        Node temp = this.head;
+
+        while (temp != null) {
+            result[index++] = temp.getValue();
+            temp = temp.getNext();
+        }
+
+        return Arrays.toString(result);
     }
 
     private class Node {
@@ -49,5 +98,10 @@ public class MyLinkedList {
         myLinkedList.add(1);
         myLinkedList.add(2);
         myLinkedList.add(3);
+
+        System.out.println(myLinkedList.toString());
+        System.out.println(myLinkedList.get(2));
+        myLinkedList.remove(1);
+        System.out.println(myLinkedList);
     }
 }
