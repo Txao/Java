4143c1ff56348205d9ce35196fdb6c52107720ac
Txao <txao@pm.me>
2021-02-11 23:17:06 +0300

    update

--- src/com/java/practice/p3/HashMapPractice.java
+++ src/com/java/practice/p3/HashMapPractice.java
@@ -6,7 +6,6 @@ import java.util.Map;
 public class HashMapPractice {
     public static void main(String[] args) {
         Map<Integer, String> map = new HashMap<>();
-
         map.put(1, "One");
         map.put(2, "Two");
         map.put(3, "Three");
