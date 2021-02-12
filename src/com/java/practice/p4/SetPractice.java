97d2b024ea153c52b8c67b35a92e4bbec064dfdc
Txao <txao@pm.me>
2021-02-13 00:09:54 +0300

    added Set

---
+++ src/com/java/practice/p4/SetPractice.java
@@ -0,0 +1,23 @@
+package com.java.practice.p4;
+
+import java.util.*;
+
+public class SetPractice {
+    public static void main(String[] args) {
+        Set<String> hashSet = new HashSet<>();
+        Set<String> linkedHSet = new LinkedHashSet<>();
+        Set<String> treeSet = new TreeSet<>();
+
+        //unique values
+    }
+
+    public static void testSet(Set<String> temp) {
+        temp.add("One");
+        temp.add("Two");
+        temp.add("Three");
+        temp.add("One");
+
+        for (String str : temp)
+            System.out.println(str);
+    }
+}
