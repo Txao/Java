b0aa03fd90ee232f2f8d002fe91bb3d5dc8550e2
Txao <txao@pm.me>
2021-03-14 16:39:58 +0300

    added new practice

---
+++ src/com/java/practice/p5/ThreadPoolPractice.java
@@ -0,0 +1,34 @@
+package com.java.practice.p5;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ThreadPoolPractice {
+    public static void main(String[] args) {
+        ExecutorService executorService = Executors.newFixedThreadPool(2);
+
+        for (int i = 0; i < 5; i++)
+            executorService.submit(new Work(i));
+
+        executorService.shutdown();
+    }
+}
+
+class Work implements Runnable {
+    private int id;
+
+    public Work(int id) {
+        this.id = id;
+    }
+
+    @Override
+    public void run() {
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        System.out.println("Work " + id + " was completed");
+    }
+}
